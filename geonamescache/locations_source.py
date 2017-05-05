b65993792f1ac787c50127cbf907060d90b249df
Michael Wu <mwu1993@users.noreply.github.com>
2017-05-05 14:22:12 -0700

    simplify (#12)

--- geonamescache/locations_source.py
+++ geonamescache/locations_source.py
@@ -46,43 +46,27 @@ class LocationsSource(object):
             u'Africa', u'Asia', u'Europe', u'Middle East'
         )
 
-    def city_search(self, city_name):
-        city_name = re.sub(r'^City\s+Of\s+', '', city_name, flags=re.IGNORECASE)
-        city_name = standardize_loc_name(city_name)
+    def _name_search(self, name, resolution=None):
+        name = standardize_loc_name(name)
         return dict(
-            (id_, loc.copy()) for id_, loc in self._locations_by_name[city_name].iteritems()
-            if loc['resolution'] == ResolutionTypes.CITY
+            (id_, loc.copy()) for id_, loc in self._locations_by_name[name].iteritems()
+            if not resolution or loc['resolution'] == resolution
         )
+
+    def city_search(self, city_name):
+        return self._name_search(city_name, ResolutionTypes.CITY)
 
     def admin_level_1_search(self, admin1_name):
-        admin1_name = standardize_loc_name(admin1_name)
-        return dict(
-            (id_, loc.copy()) for id_, loc in self._locations_by_name[admin1_name].iteritems()
-            if loc['resolution'] == ResolutionTypes.ADMIN_1
-        )
+        return self._name_search(admin1_name, ResolutionTypes.ADMIN_1)
 
     def admin_level_2_search(self, admin2_name):
-        admin2_name = standardize_loc_name(admin2_name)
-        return dict(
-            (id_, loc.copy()) for id_, loc in self._locations_by_name[admin2_name].iteritems()
-            if loc['resolution'] == ResolutionTypes.ADMIN_2
-        )
+        return self._name_search(admin2_name, ResolutionTypes.ADMIN_2)
 
     def country_search(self, country_name):
-        country_name = standardize_loc_name(country_name)
-        return dict(
-            (id_, loc.copy()) for id_, loc in self._locations_by_name[country_name].iteritems()
-            if loc['resolution'] == ResolutionTypes.COUNTRY
-        )
+        return self._name_search(country_name, ResolutionTypes.COUNTRY)
 
     def all_locations_search(self, name):
-        matches = {}
-        for lookup in (
-            self.city_search, self.admin_level_1_search, self.admin_level_2_search,
-            self.country_search,
-        ):
-            matches.update(lookup(name))
-        return matches
+        return self._name_search(name)
 
     def get_location_by_id(self, id_):
         return self._locations_by_id[id_].copy()
